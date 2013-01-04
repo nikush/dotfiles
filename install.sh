15d0d9d859d1a4cbcee5014910f4b4829b1f8a14
Nikush Patel <nikush.patel7@gmail.com>
2013-01-04 12:00:09 +0000

    Add install and uninstall scripts.

---
+++ install.sh
@@ -0,0 +1,37 @@
+# Title       : install.sh
+# Date        : 2013-01-04
+# Author      : Nikush Patel
+# Version     : 1.0
+# Description : Install dotfiles from the .dotfiles repository
+# Options     : None
+
+if [ ! -e ~/.vimrc ]; then
+    ln -s ~/.dotfiles/vimrc ~/.vimrc
+else
+    printf "\e[1m%s\e[0m %s\n" ".vimrc" "file already present. Skipped."
+fi
+
+if [ ! -e ~/.vim ]; then
+    ln -s ~/.dotfiles/vim ~/.vim
+else
+    printf "\e[1m%s\e[0m %s\n" ".vim" "directory already present. Skipped."
+fi
+
+if [ -e ~/.bashrc ]; then
+    printf "%s" ".bashrc file already present, append line to source bashrc file from dotfiles repository? (y/n) "
+    read response
+    if [ -n "$response" ] && [ $response = "y" ]; then
+        bashrc_source=". ~/.dotfiles/bashrc"
+        printf "\n\n%s" "$bashrc_source" >> ~/.bashrc
+    fi
+else
+    ln -s ~/.dotfiles/bashrc ~/.bashrc
+fi
+
+printf "%s" "Initialise vim plugin submodules? (y/n) "
+read response
+if [ -n "$response" ] && [ $response = "y" ]; then
+    git submodule update --init
+fi
+
+printf "\e[32m%s\e[0m\n" "Installation complete!"
---
+++ uninstall.sh
@@ -0,0 +1,36 @@
+# Title       : uninstall.sh
+# Date        : 2013-01-04
+# Author      : Nikush Patel
+# Version     : 1.0
+# Description : Uninstall dotfiles linked to the .dotfiles repository
+# Options     : None
+
+if [ -e ~/.vimrc ]; then
+    if [ -L ~/.vimrc ]; then
+        rm ~/.vimrc
+    else
+        printf "%s\n" ".vimrc is not a symlink. Skipped."
+    fi
+else
+    printf "%s\n" ".vimrc doesn't exist. Skipped."
+fi
+
+if [ -e ~/.vim ]; then
+    if [ -L ~/.vim ]; then
+        rm ~/.vim
+    else
+        printf "%s\n" ".vim is not a symlink. Skipped."
+    fi
+else
+    printf "%s\n" ".vim doesn't exist. Skipped."
+fi
+
+if [ -e ~/.bashrc ]; then
+    if [ -L ~/.bashrc ]; then
+        rm ~/.bashrc
+    else
+        printf "%s\n> \e[1m%s\e[0m\n" "Don't forget to remove the line from your bashrc file that sources the bashrc from the dotfiles repository:" ". ~/.dotfiles/bashrc"
+    fi
+fi
+
+printf "\e[32m%s\e[0m\n" "Uninstallation complete!"
